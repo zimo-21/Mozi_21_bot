17d12fb7ee476bd434802b6a73913da8d06c718e
zimo-21 <abuziyaan09@gmail.com>
2026-02-17 09:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 TELEGRAM_TOKEN=os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY= os.getenv("GEMINI_API_KEY")
  #Gemini AI Setup
-genai.configure(api_key=os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Logging
